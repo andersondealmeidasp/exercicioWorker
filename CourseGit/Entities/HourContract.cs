48f7daf0e231be4f0b80c4764f0a2ea29860ca16
anderson.almeida <andersondealmeidasp@gmail.com>
2019-02-11 19:31:39 -0200

    primeiro commit

---
+++ CourseGit/Entities/HourContract.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseGit.Entities
+{
+    class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract()
+        {
+
+        }
+
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            this.Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
